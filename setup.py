e41410c7b3988fc2f1a6710288e484d211981102
Evren <evren.pamir@zalando.de>
2017-07-11 14:30:36 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='myPackage',
+    name='simpsons',
+    version='0.0',
+    description='Character recognition for simpsons image data',
+    author='evren',
     packages=find_packages(),
 )
